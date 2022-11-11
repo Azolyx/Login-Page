e6000ccf269101b31f4f78a6eea9484675028c31
Azolyx <108442846+Azolyx@users.noreply.github.com>
2022-11-11 14:39:06 -0600

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_host = 'DB Host';
-$db_name = 'DB Name';
-$db_username = 'DB Username';
-$db_password = 'DB Password';
-$dsn = "mysql:host=$db_host;dbname=$db_name";
-$options = array();
-?>
